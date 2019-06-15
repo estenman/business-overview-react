a6c6f3aa1981fe309b0a2494cdca8c643323adcb
Ericka Swift <23269503+estenman@users.noreply.github.com>
2019-06-15 16:12:06 -0700

    Add new program button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import './Normalize.css'
-import './components/header/Header-FullNavigation.css';
-import Header from './components/header/Header.js';
+import './components/Header/Header-FullNavigation.css';
+import './components/Program-Button/ProgramButton.css';
+import Header from './components/Header/Header.js';
+import ProgramButton from './components/Program-Button/ProgramButton.js';
 
 function App() {
   return (
-    <div>
+    <div className="page-wrapper">
     	<Header />
+    	<div className="program-wrapper">
+    		 <ProgramButton />
+    	</div>
     </div>
   );
 }
---
+++ src/components/Program-Button/ProgramButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ProgramButton extends Component {
+	render() {
+		return(
+			<aside>
+				<button aria-label="New Program" aria-describedby="descriptionNewProgram" className="program-button">
+					<img src="/assets/plus_icon.png" alt="plus sign" />
+				</button>
+				<p className="program-button-copy">New Program</p>
+				<div id="descriptionNewProgram">Clicking the button will open a window to add a new program</div>
+			</aside>
+		);
+	}
+}
+
+export default ProgramButton;
